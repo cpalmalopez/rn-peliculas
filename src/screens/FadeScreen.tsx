184502a7138d8c0cc93c22e6a7d39632f9ab8c3b
Claudio Palma López <xclaudio@outlook.com>
2021-04-20 17:53:57 -0400

    fix FadeScreen

--- src/screens/FadeScreen.tsx
+++ src/screens/FadeScreen.tsx
@@ -25,11 +25,11 @@ export const FadeScreen = () => {
 
             <Button
                 title="FadeIn"
-                onPress={fadeIn}
+                onPress={() => fadeIn()}
             />
             <Button
                 title="FadeOut"
-                onPress={fadeOut}
+                onPress={() => fadeOut()}
             />
         </View>
     )
